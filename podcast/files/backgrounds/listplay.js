12d930f99d43eb4a8ec80520afb5a71173b73f0a
Maxim <mr.max380@ya.ru>
2019-02-12 20:02:30 +0300

    ver 4

---
+++ podcast/files/backgrounds/listplay.js
@@ -0,0 +1,89 @@
+"use strict";
+    $(function () {
+        $('.mplayer').mplayer({
+            'volume': 80,
+            'playlist': [
+
+                {
+                    "title": "Третий выпуск подкаста (нас уже двое)",
+                    "author": "1",
+                    "cover": "podcast/files/covers/1.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/1.mp3",
+                    "duration": "232"
+                },
+                {
+                    "title": "Выход Nintendo Switch!",
+                    "author": "2",
+                    "cover": "podcast/files/covers/2.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/2.mp3",
+                    "duration": "221"
+                },
+                {
+                    "title": "Ребят, а мы ж так и не представились!",
+                    "author": "3",
+                    "cover": "podcast/files/covers/3.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/3.mp3",
+                    "duration": "233"
+                },
+                {
+                    "title": "Google, WebAPK, AliExpress и чуток про большую N",
+                    "author": "4",
+                    "cover": "podcast/files/covers/4.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/4.mp3",
+                    "duration": "236"
+                },
+                {
+                    "title": "Don't Let Me Down",
+                    "author": "The Chainsmokers",
+                    "cover": "podcast/files/covers/5.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/5.mp3",
+                    "duration": "219"
+                },
+                {
+                    "title": "Mind",
+                    "author": "Skrillex & Diplo",
+                    "cover": "podcast/files/covers/6.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/6.mp3",
+                    "duration": "211"
+                },
+                {
+                    "title": "We Don't Talk Anymore",
+                    "author": "Charlie Puth",
+                    "cover": "podcast/files/covers/7.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/7.mp3",
+                    "duration": "217"
+                },
+                {
+                    "title": "Chained To The Rhythm",
+                    "author": "Katy Perry",
+                    "cover": "podcast/files/covers/8.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/8.mp3",
+                    "duration": "237"
+                },
+                {
+                    "title": "Kamikaze",
+                    "author": "MØ",
+                    "cover": "podcast/files/covers/9.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/9.mp3",
+                    "duration": "205"
+                },
+                {
+                    "title": "I Got You",
+                    "author": "Bebe Rexha",
+                    "cover": "podcast/files/covers/10.jpg",
+                    "background": "podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/10.mp3",
+                    "duration": "191"
+                }
+            ]
+        });
+    });
