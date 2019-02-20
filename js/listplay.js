700a1696986042aa9e1df90aa09900091a74e267
Maxim <mr.max380@ya.ru>
2019-02-20 06:34:50 +0300

    ver 7

---
+++ js/listplay.js
@@ -0,0 +1,337 @@
+"use strict";
+    $(function () {
+        $('.mplayer').mplayer({
+            'volume': 100,
+            'playlist': [
+
+                {
+                    "title": "Третий выпуск подкаста (нас уже двое)",
+                    "author": "1",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/01.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Выход Nintendo Switch!",
+                    "author": "2",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/02.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Ребят, а мы ж так и не представились!",
+                    "author": "3",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/03.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Google, WebAPK, AliExpress и чуток про большую N",
+                    "author": "4",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/04.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Хей-хей! Xiaomi",
+                    "author": "5",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/05.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Android Pay, чужой завет...",
+                    "author": "6",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/06.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Взрослое аниме, Windows 10",
+                    "author": "7",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/07.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Сплиткаст! Новая рубрика...",
+                    "author": "8",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/08.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Сплиткаст! Самая лучшая игра...",
+                    "author": "9",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/09.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Сплиткаст! Вещает Рабадан...",
+                    "author": "10",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/10.mp3",
+                    "duration": ""  
+                },
+				                {
+                    "title": "Новая игра во вселенной Чужих: Надежда Хадли - глоток свежего воздуха после Завета",
+                    "author": "11",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/11.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Ньюскаст! Пилотный выпуск со словами паразитами: Вот, вот... вооот!!!",
+                    "author": "12",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/12.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Нанокаст! Новый вид развода на AliExpress?!",
+                    "author": "13",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/13.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "ЧетыреSкаст! The Legend of Zelda: Breath of the Wild... Честное мнение",
+                    "author": "14",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/14.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Рассказываю про Харлана Эллисона",
+                    "author": "15",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/15.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Как заработать в интернете - вся правда о торговых площадках и их методах работы",
+                    "author": "16",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/16.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Что такое RSS лента?",
+                    "author": "17",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/17.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Мнение фаната Nintendo о консоли Switch (2 недели эксплуатации)",
+                    "author": "18",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/18.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Что такое подкаст? и зачем вы нас слушаете =)",
+                    "author": "19",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/19.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "О медицине - мой неудачный опыт =(",
+                    "author": "20",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/20.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Игровые смартфоны и посылки из Китая",
+                    "author": "21",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/21.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Windows 95 в часах и заговор разработчиков игр",
+                    "author": "22",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/22.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Red Dead Redemption 2, Metro Exodus и Android",
+                    "author": "23",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/23.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Все игры на Android по подписке и Diablo 4 на мобилках",
+                    "author": "24",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/24.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "МегаФон - мне не хотят возвращать мои день",
+                    "author": "25",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/25.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Честно об играх - Konami хочет уничтожить PT",
+                    "author": "26",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/26.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Честно об играх - Не пройти игру до конца - это норма?",
+                    "author": "27",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/27.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Выход нового Nexus телефона в 2019",
+                    "author": "28",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/28.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Мощнейший процессор на Android, Siri без интернета",
+                    "author": "29",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/29.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Перезапуск! Черная пятница",
+                    "author": "30",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/30.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Бюджетный iPhone X, Fallout 76 (защищаем =)",
+                    "author": "31",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/31.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Red Dead Redemption 2 - толковый шмот за реальные деньги",
+                    "author": "32",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/32.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Вся правда о Fallout 76, проблемы Яндекс Дзен",
+                    "author": "33",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/33.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Pornhub 2018, док превращающий телефон в ПК",
+                    "author": "34",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/34.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Battle of Titans - супер игра про мехов на Android",
+                    "author": "35",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/35.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Солдат 76 гей... Google Play больше не нужен!",
+                    "author": "36",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/36.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Special! Вся правда об ЭВМ 80-90-х",
+                    "author": "37",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/37.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "RTX в играх, PlayStation 5 уже у разработчиков",
+                    "author": "38",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/38.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Apex Legends - все об игре, а Google дарит деньги!",
+                    "author": "39",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/39.mp3",
+                    "duration": ""
+                },
+                {
+                    "title": "Что такое PMGRM? - как раскрутить Инстаграм",
+                    "author": "40",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/40.mp3",
+                    "duration": ""
+                },
+				                {
+                    "title": "Профессия киберспортсмен, NFC в Китае и Крыму не любят!",
+                    "author": "41",
+                    "cover": "./podcast/files/covers/fon.png",
+                    "background": "./podcast/files/backgrounds/fon.jpg",
+                    "pfile": "http://s168996.smrtp.ru/media/podcast/41.mp3",
+                    "duration": ""
+                }
+            ]
+        });
+    });
