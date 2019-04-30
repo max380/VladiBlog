f5d9e91e29eb2c4bcd89c32a358253ac8b1705fc
Maxim <mr.max380@ya.ru>
2019-04-30 08:17:34 +0300

    Revert "ver 2.6"
    This reverts commit 46ca476c7926832a552a1c9da8330af033d5e98c.

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@
 
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
-  window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
   };
 
   window.plugins.OneSignal
-    .startInit("9b3026e2-b9f0-47f6-adcb-d021e0acfa60")
+    .startInit("547585eb-d8aa-4152-8062-a74e74efde9c")
     .handleNotificationOpened(notificationOpenedCallback)
     .endInit();
 }, false);
